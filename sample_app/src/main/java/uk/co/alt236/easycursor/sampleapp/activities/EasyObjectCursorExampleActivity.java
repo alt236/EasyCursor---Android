ae5eb40bf4f5c42ffe64d72550898bbfa8f77514
Alexandros Schillings <aschillings@gmail.com>
2015-07-23 17:09:07 +0100

    Added APACHE-2 headers

--- sample_app/src/main/java/uk/co/alt236/easycursor/sampleapp/activities/EasyObjectCursorExampleActivity.java
+++ sample_app/src/main/java/uk/co/alt236/easycursor/sampleapp/activities/EasyObjectCursorExampleActivity.java
@@ -1,10 +1,10 @@
-/**
- * ****************************************************************************
- * Copyright 2013 Alexandros Schillings
+/*
+ * ***************************************************************************
+ * Copyright 2015 Alexandros Schillings
  * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *  You may obtain a copy of the License at
  * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
  * <p>
@@ -13,8 +13,10 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * ****************************************************************************
+ * ***************************************************************************
+ *
  */
+
 package uk.co.alt236.easycursor.sampleapp.activities;
 
 import android.database.Cursor;
